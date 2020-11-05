a35a0e0425f307ef5380c71afff2ffdede3e8b86
Bob <maydaybob2000@gmail.com>
2020-11-05 21:56:15 +0800

    Test API place order

--- rest/order.js
+++ rest/order.js
@@ -8,6 +8,39 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 const client = new RestClient(API_KEY, PRIVATE_KEY)
 
+const placeActiveOrder = ({
+  side,
+  symbol,
+  order_type,
+  price,
+  qty,
+  time_in_force,
+  reduce_only,
+  close_on_trigger,
+}) => {
+  client
+    .placeActiveOrder({
+      side,
+      symbol,
+      order_type,
+      price,
+      qty,
+      time_in_force,
+      reduce_only,
+      close_on_trigger,
+    })
+    .then((data) => {
+      if (data['ret_msg'].toString().toUpperCase() === 'OK') {
+        console.log('OK', data)
+      } else {
+        console.log('somethings wrong:', data)
+      }
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
+
 const getActiveOrder = () => {
   client
     .getActiveOrder()
@@ -40,7 +73,53 @@ const cancelActiveOrder = ({ symbol: symbol, order_id: order_id }) => {
     })
 }
 
-cancelActiveOrder({ symbol: 'BTCUSD', order_id: 'c4647b56-10e9-47f5-95a3-1ce94eea441e' })
+// cancelActiveOrder({ symbol: 'BTCUSD', order_id: 'c4647b56-10e9-47f5-95a3-1ce94eea441e' })
+
+// placeActiveOrder({
+//   side: 'Buy',
+//   symbol: 'BTCUSD',
+//   order_type: 'Limit',
+//   price: 9666,
+//   qty: 8,
+//   time_in_force: 'GoodTillCancel',
+//   reduce_only: false,
+//   close_on_trigger: false,
+// })
 // module.exports = {
 //   getActiveOrder: getActiveOrder,
+// }
+
+getActiveOrder()
+
+// Place Order API respons =>
+// OK {
+//   ret_code: 0,
+//   ret_msg: 'OK',
+//   ext_code: '',
+//   ext_info: '',
+//   result: {
+//     user_id: 131263,
+//     order_id: '106cf585-a609-4cce-bb3e-74a056cd7418',
+//     symbol: 'BTCUSD',
+//     side: 'Buy',
+//     order_type: 'Limit',
+//     price: 9666,
+//     qty: 8,
+//     time_in_force: 'GoodTillCancel',
+//     order_status: 'Created',
+//     last_exec_time: 0,
+//     last_exec_price: 0,
+//     leaves_qty: 8,
+//     cum_exec_qty: 0,
+//     cum_exec_value: 0,
+//     cum_exec_fee: 0,
+//     reject_reason: 'EC_NoError',
+//     order_link_id: '',
+//     created_at: '2020-11-05T13:54:17.074Z',
+//     updated_at: '2020-11-05T13:54:17.074Z'
+//   },
+//   time_now: '1604584457.075340',
+//   rate_limit_status: 99,
+//   rate_limit_reset_ms: 1604584457072,
+//   rate_limit: 100
 // }
--- websocket/websocket.js
+++ websocket/websocket.js
@@ -164,3 +164,68 @@ module.exports = {
 //     }
 //   ]
 // }
+
+// Place Order =>
+//
+// update {
+//   topic: 'position',
+//   data: [
+//     {
+//       user_id: 131263,
+//       symbol: 'BTCUSD',
+//       size: 0,
+//       side: 'None',
+//       position_value: '0',
+//       entry_price: '0',
+//       liq_price: '0',
+//       bust_price: '0',
+//       leverage: '3',
+//       order_margin: '0.0006501',
+//       position_margin: '0',
+//       available_balance: '0.0012099',
+//       take_profit: '0',
+//       stop_loss: '0',
+//       realised_pnl: '0',
+//       trailing_stop: '0',
+//       trailing_active: '0',
+//       wallet_balance: '0.00186',
+//       risk_id: 1,
+//       occ_closing_fee: '0',
+//       occ_funding_fee: '0',
+//       auto_add_margin: 1,
+//       cum_realised_pnl: '0',
+//       position_status: 'Normal',
+//       position_seq: 0,
+//       Isolated: true,
+//       mode: 0,
+//       position_idx: 0
+//     }
+//   ]
+// }
+// update {
+//   topic: 'order',
+//   data: [
+//     {
+//       order_id: '106cf585-a609-4cce-bb3e-74a056cd7418',
+//       order_link_id: '',
+//       symbol: 'BTCUSD',
+//       side: 'Buy',
+//       order_type: 'Limit',
+//       price: '9666',
+//       qty: 8,
+//       time_in_force: 'GoodTillCancel',
+//       create_type: 'CreateByUser',
+//       cancel_type: '',
+//       order_status: 'New',
+//       leaves_qty: 8,
+//       cum_exec_qty: 0,
+//       cum_exec_value: '0',
+//       cum_exec_fee: '0',
+//       timestamp: '2020-11-05T13:54:17.074Z',
+//       take_profit: '0',
+//       stop_loss: '0',
+//       trailing_stop: '0',
+//       last_exec_price: '0'
+//     }
+//   ]
+// }
